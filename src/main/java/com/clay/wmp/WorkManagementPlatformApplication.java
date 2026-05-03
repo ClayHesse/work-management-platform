68375ca9946007a65980f69de58113b6fca0d162
Clay Hesse <CWolf445@gmail.com>
2026-05-03 17:43:15 -0400

    Initial project setup

---
+++ src/main/java/com/clay/wmp/WorkManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.clay.wmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkManagementPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clay/wmp/WorkManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clay.wmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
